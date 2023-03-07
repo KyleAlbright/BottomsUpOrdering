c804c2333ba85d642ef8acbef824b4296a3bd98e
jbkoshy <koshyjeremy@gmail.com>
2023-03-06 21:02:51 -0600

    fixing contact form to send email

--- client/src/pages/ContactForm.js
+++ client/src/pages/ContactForm.js
@@ -1,45 +1,106 @@
-import { FormControl, FormGroup, InputLabel, Input, Button, makeStyles } from "@material-ui/core";
-import React from "react";
+import React from 'react';
+import { Box, makeStyles, TextField , Button, TextareaAutosize } from '@material-ui/core'
+import emailjs from '@emailjs/browser'
+import Swal from 'sweetalert2'
 
-const useStyle=makeStyles({
-    formStyle: {
-        width: "50%",
-        margin: 'auto',
-        padding: 20,
-        paddingTop: 20,
-        boxShadow: "0px 0px 10px rgba(0,0,0,0.5)",
+// css
+
+const useStyles = makeStyles(() => ({
+    loginscreen:{
+        display:"flex",
+        justifyContent:"center",
+        alignItems:'center',
+        maxHeight:"30vh",
+        textAlign:"center",
+        margin:"160px",
+
     },
-    myBtn: {
-        marginTop: 10,
-        width: "10%",
+    text:{
+        width:"400px",
+        height:"5rem",
+        lineHeight: "1.6em"
     },
-    header: {
-    textAlign: "center"
+    btnLogin:{
+        lineHeight: "2.55rem",
+        letterSpacing:"1em",
+        textAlign:"center",
+        fontWeight:"bold",
+        textDecoration:"none",
+        color:"#fff",
+        backgroundColor: "#291e38",
+        marginTop:"15px"
+    },
+    btnText:{
+        color:"#fff",
     }
-});
+}))
 
-export default function Contact() {
-    const classes = useStyle();
-    return (
-        <div>
-            <h1 className={classes.header}>Contact Form</h1>
-            <FormGroup className={classes.formStyle}>
-                <FormControl>
-                    <InputLabel>Full Name</InputLabel>
-                    <Input />
-                </FormControl>
-                <FormControl>
-                    <InputLabel>Email</InputLabel>
-                    <Input />
-                </FormControl>
-                <FormControl>
-                    <InputLabel>Message</InputLabel>
-                    <Input />
-                </FormControl>
-                <Button variant="contained" color="secondary" className={classes.myBtn}>
-                    Send
-                </Button>
-            </FormGroup>
-        </div>
-    );
+function App(){
+  const classes = useStyles();
+  const onHandleSubmit= (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_rfe2vim', 'template_r1l66kl', e.target, 'M8aO5_Npmhdub5SrD')
+        .then((result) => {
+          console.log(result.text);
+          Swal.fire({
+            icon:'success',
+            title:'Success! Message sent.'
+          })
+        }, (error) => {
+          console.log(error.text);
+          Swal.fire({
+            icon:'error',
+            title:"Error! Try again.",
+            text:error.text,
+          })
+        });
+        e.target.reset();
+    };
+
+  return (
+    <>
+      <Box className = {classes.loginscreen}>
+        <form  onSubmit={onHandleSubmit}>
+            <div>
+                 <h2>Contact Form</h2>
+                <TextField fullWidth className={classes.text}
+                  type="text"
+                  label="Name"
+                  required
+                  name="user_name"
+                />
+            </div>
+
+            <div>
+                <TextField className={classes.text}
+                    type="email"
+                    label="Email"
+                    name="user_email"
+                    required
+                />
+            </div>
+
+            <div >
+                <TextareaAutosize className={classes.text}
+                    type="text"
+                     label="Message"
+                    required
+                    name="message"
+                    variant="outlined"
+                    placeholder='Fill out your message here and click submit!'
+                    style={{ width: 400 , height:100, borderColor:"#909090", borderWidth:1.3, 
+                    marginBottom:"10px"}}
+                />
+            </div>
+
+            <div className={classes.btnLogin}>
+                <Button type='submit' className={classes.btnText}>SUBMIT</Button>
+            </div> 
+        </form>
+    </Box>
+    </>
+  )
 }
+
+export default App;
